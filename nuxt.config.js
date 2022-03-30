be749828c23d83e24f0573b6c743c759465a0fe5
younoch-devxhub <git.younoch@devxhub.com>
2022-03-30 06:47:43 +0600

    nuxt config serave removed

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,9 +76,9 @@ export default {
       },
     },
   },
-  server: {
-    port: process.env.PORT ||8000, // default: 3000
-    host: '0.0.0.0', // default: localhost,
-    timing: false
-  }
+  // server: {
+  //   port: process.env.PORT ||8000, // default: 3000
+  //   host: '0.0.0.0', // default: localhost,
+  //   timing: false
+  // }
 }
