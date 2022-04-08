de1e44b04a61e67b58ebcc8d45b00d0656d7056e
younoch-devxhub <git.younoch@devxhub.com>
2022-04-09 02:34:28 +0600

    add meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { property: 'og:type', content: 'website' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'favicon/favicon.ico' }
