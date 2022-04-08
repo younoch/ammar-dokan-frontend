9cc77e8aedfdcf30b6a89457a46ab709e4189408
younoch-devxhub <git.younoch@devxhub.com>
2022-04-08 23:38:02 +0600

    dsd

--- components/Login.vue
+++ components/Login.vue
@@ -10,7 +10,7 @@
             <span class="text-3xl font-extrabold text-indigo-600 uppercase">Amar Dhokan</span>
           </h1>
           <div class="flex-1 w-full mt-6 lg:mt-8">
-            <!--             <div class="flex flex-col items-center">
+            <!-- <div class="flex flex-col items-center">
               <button
                 class="flex items-center justify-center w-full max-w-xs py-3 font-bold text-gray-800 transition-all duration-300 ease-in-out bg-indigo-100 rounded-lg shadow-sm focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline"
               >
@@ -72,20 +72,7 @@
                 <input
                   id="email"
                   v-model.trim="login.email"
-                  class="
-                    w-full
-                    px-6
-                    py-3
-                    text-sm
-                    font-medium
-                    placeholder-gray-500
-                    bg-gray-100
-                    border border-gray-200
-                    rounded-lg
-                    lg:px-8 lg:py-4
-                    focus:outline-none focus:border-gray-400 focus:bg-white
-                    form__input
-                  "
+                  class="w-full px-6 py-3 text-sm font-medium placeholder-gray-500 bg-gray-100 border border-gray-200 rounded-lg  lg:px-8 lg:py-4 focus:outline-none focus:border-gray-400 focus:bg-white form__input"
                   type="email"
                   placeholder="Email"
                   @input="setEmail($event.target.value)"
@@ -109,19 +96,7 @@
                 >Password</label>
                 <input
                   v-model="login.password"
-                  class="
-                    w-full
-                    px-6
-                    py-3
-                    text-sm
-                    font-medium
-                    placeholder-gray-500
-                    bg-gray-100
-                    border border-gray-200
-                    rounded-lg
-                    lg:px-8 lg:py-4
-                    focus:outline-none focus:border-gray-400 focus:bg-white
-                  "
+                  class="w-full px-6 py-3 text-sm font-medium placeholder-gray-500 bg-gray-100 border border-gray-200 rounded-lg  lg:px-8 lg:py-4 focus:outline-none focus:border-gray-400 focus:bg-white"
                   type="password"
                   placeholder="Password"
                   @input="setPassword($event.target.value)"
@@ -149,26 +124,7 @@
               </div> -->
               <button
                 type="submit"
-                class="
-                  flex
-                  items-center
-                  justify-center
-                  w-full
-                  py-2
-                  mt-5
-                  font-semibold
-                  tracking-wide
-                  text-gray-100
-                  transition-all
-                  duration-300
-                  ease-in-out
-                  bg-indigo-500
-                  disabled:bg-indigo-200
-                  rounded-lg
-                  lg:py-3
-                  hover:bg-indigo-700
-                  focus:shadow-outline focus:outline-none
-                "
+                class="flex items-center justify-center w-full py-2 mt-5 font-semibold tracking-wide text-gray-100 transition-all duration-300 ease-in-out bg-indigo-500 rounded-lg  disabled:bg-indigo-200 lg:py-3 hover:bg-indigo-700 focus:shadow-outline focus:outline-none"
                 :disabled="$v.$invalid"
               >
                 <span v-if="hideLoadingIcon" class="text-xl mdi mdi-login-variant" />
@@ -177,12 +133,7 @@
               </button>
 
               <div
-                class="
-                  mt-2
-                  text-center text-gray-600
-                  lg:mt-10 lg:text-lg
-                  inline
-                "
+                class="inline mt-2 text-center text-gray-600  lg:mt-10 lg:text-lg"
               >
                 <span class="inline text-sm">Don't have an account yet?</span>
                 <SignUp class="inline cursor-pointer" />
@@ -209,7 +160,7 @@ export default {
   name: 'SignIn',
   data () {
     return {
-      loadingIcon: `<svg role="status" class="inline mr-3 w-5 h-5 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+      loadingIcon: `<svg role="status" class="inline w-5 h-5 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"></path>
 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"></path>
 </svg>`,
